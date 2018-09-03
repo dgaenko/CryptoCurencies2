4c14742d984ffa46139fd11b8bc1c903955ad022
Dmitry Gaenko <d.gaenko@gmail.com>
2018-09-03 09:52:24 +0300

    Last commit

--- app/src/main/java/ru/familion/cryptocurencies/CurrenciesApp.kt
+++ app/src/main/java/ru/familion/cryptocurencies/CurrenciesApp.kt
@@ -18,9 +18,9 @@ class CurrenciesApp: Application(), HasActivityInjector {
 
     override fun onCreate() {
         super.onCreate()
-        //if (BuildConfig.DEBUG) {
+        if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
-        //}
+        }
         AppInjector.init(this)
     }
 
